7feb049c65df5d56b0b0cad4d02aae72a41f9ba1
Yusuf Turhalli <yusufturhalli@gmail.com>
2024-08-06 14:44:48 +0300

    Add initial files

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h3>Editable with default</h3>
+    <RateBox v-model="rating" :defaultValue="3" />
+    <p>Selected Rating: {{ rating }}</p>
+    <h3>Editable without default</h3>
+    <RateBox v-model="withoutDefaultRating" />
+    <p>Selected Rating: {{ withoutDefaultRating }}</p>
+
+    <h3>Not editable</h3>
+    <RateBox :modelValue="4" :editable="false" />
+    <p>Non-editable Rating: 4</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import RateBox from "./components/RateBox.vue";
+
+const rating = ref(0);
+const withoutDefaultRating = ref(0);
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RateBox.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="rate-box">
+    <span
+      v-for="star in 5"
+      :key="star"
+      class="star"
+      :class="{ filled: star <= (hoveredStar || internalValue) }"
+      @mouseover="onMouseOver(star)"
+      @mouseout="onMouseOut"
+      @click="onClick(star)"
+    >
+      ★
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits, watch, onMounted } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    required: true,
+  },
+  editable: {
+    type: Boolean,
+    default: true,
+  },
+  defaultValue: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+const hoveredStar = ref(null);
+const internalValue = ref(props.modelValue || props.defaultValue);
+
+onMounted(() => {
+  if (props.modelValue === 0 && props.defaultValue !== 0) {
+    emit('update:modelValue', props.defaultValue);
+  }
+});
+
+watch(() => props.modelValue, (newValue) => {
+  internalValue.value = newValue;
+});
+
+const onMouseOver = (star) => {
+  if (props.editable) {
+    hoveredStar.value = star;
+  }
+};
+
+const onMouseOut = () => {
+  if (props.editable) {
+    hoveredStar.value = null;
+  }
+};
+
+const onClick = (star) => {
+  if (props.editable) {
+    const newValue = props.modelValue === star ? 0 : star;
+    emit('update:modelValue', newValue);
+  }
+};
+</script>
+
+<style scoped>
+.rate-box {
+  display: inline-flex;
+}
+.star {
+  font-size: 2rem;
+  color: #ccc;
+  cursor: pointer;
+}
+.star.filled {
+  color: #f39c12;
+}
+.star:hover {
+  color: #f39c12;
+}
+.star[editable="false"] {
+  cursor: default;
+}
+</style>
